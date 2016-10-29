96e3686d9269fb272af13d03f02e87408d44aa81
Zarkana <jsc940@gmail.com>
2016-10-28 22:08:22 -1000

    Added complete crud resources for interfaces

--- app/controllers/interfaces_controller.rb
+++ app/controllers/interfaces_controller.rb
@@ -10,11 +10,32 @@ class InterfacesController < ApplicationController
   def new
   end
 
+  def edit
+    @interface = Interface.find(params[:id])
+  end
+
   def create
     @interface = Interface.new(interface_params)
 
     @interface.save
     redirect_to @interface
+  end
+
+  def update
+    @interface = Interface.find(params[:id])
+
+    if @interface.update(interface_params)
+      redirect_to @interface
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @interface = Interface.find(params[:id])
+    @interface.destroy
+
+    redirect_to interfaces_path
   end
 
   private
